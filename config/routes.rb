9bb1c75b0f93ae163aef9e787445674921975f7b
Junghyun Yoon <junghyunyoon@Junghyun-Yoons-iMac.local>
2011-01-17 13:39:29 +0900

    Removed default Rails page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 SosoMoim::Application.routes.draw do
+  get "users/new"
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
-
-
+  
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
+  root :to => 'pages#home'
+  
+  
+  
 end
