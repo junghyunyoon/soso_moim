52960d6a3a2e56f25897a244a6ba20524effb6b4
Junghyun Yoon <junghyunyoon@Junghyun-Yoons-iMac.local>
2011-01-17 10:48:42 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SosoMoim::Application.config.secret_token = '8efecd93835fd38406a85bfc91ec6a935463aaa336dcf74ad6a5469c7f7f945f1a86bbba6f13d8c35e8e73e2dddb67ea829ec88040bb966c9859c710a4a8ac9c'
