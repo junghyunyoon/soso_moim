8da2c20668ebd54af1df40d1d33f5f592d3c18e7
Junghyun Yoon <junghyunyoon@Junghyun-Yoons-iMac.local>
2011-01-17 11:36:57 +0900

    updated

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return a title on a per-page basis.
+  def title
+    base_title = "HeadFlow Sosohan Moim"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
